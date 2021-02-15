6daa120e6960c23c85227ca22746eb7f6f913b92
Kenny Sherman <kennethsherm@gmail.com>
2021-02-14 21:08:51 -0600

    normalizing validation data

--- project1Main.py
+++ project1Main.py
@@ -14,9 +14,10 @@ Y = data['Y']
 xTr, xTv, yTr, yTv = valsplit(X, Y)
 
 xTr_normed = xTr / xTr.max(axis=0)
+xTv_normed = xTv / xTv.max(axis=0)
 
 # train spam filter with settings and parameters in trainspamfilter.py
 w_trained = trainspamfilter(xTr_normed, yTr)
 
 # evaluate spam filter on test set using default threshold
-spamfilter(xTv, yTv, w_trained)
+spamfilter(xTv_normed, yTv, w_trained)
