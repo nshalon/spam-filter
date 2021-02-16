dea53f212156475b5a3e8e25ccac7e92b7d2159a
Kenny Sherman <kennethsherm@gmail.com>
2021-02-15 20:15:02 -0600

    sherman hinge implementation

--- hinge.py
+++ hinge.py
@@ -4,23 +4,24 @@ from checkgradHingeAndRidge import checkgradHingeAndRidge
 
 
 def hinge(w,xTr,yTr,lambdaa):
-#
-#
-# INPUT:
-# xTr dxn matrix (each column is an input vector)
-# yTr 1xn matrix (each entry is a label)
-# lambda: regularization constant
-# w weight vector (default w=0)
-#
-# OUTPUTS:
-#
-# loss = the total loss obtained with w on xTr and yTr
-# gradient = the gradient at w
+    #
+    #
+    # INPUT:
+    # xTr dxn matrix (each column is an input vector)
+    # yTr 1xn matrix (each entry is a label)
+    # lambda: regularization constant
+    # w weight vector (default w=0)
+    #
+    # OUTPUTS:
+    #
+    # loss = the total loss obtained with w on xTr and yTr
+    # gradient = the gradient at w
 
     pred_z = np.sum(xTr.T * w.T, axis=1) * yTr
-    gradient = lambdaa * np.sum((xTr * yTr) * (pred_z >= 1), axis=1) + (2 * w)
+    gradient = lambdaa * np.sum((xTr * -yTr) * (pred_z < 1), axis=1).reshape((xTr.shape[0], 1))
+    gradient += (2 * w)
 
-    loss = lambdaa * np.sum(np.maximum(1 - pred_z, np.zeros(xTr.shape[1]))) + np.linalg.norm(w)
+    loss = lambdaa * np.sum(np.maximum(1 - pred_z, np.zeros(xTr.shape[1]))) + (np.linalg.norm(w) ** 2)
 
     # pred_z = np.multiply(yTr, (np.asmatrix(w.T) * xTr)) # 1 x n
     # emp_loss_nonzerod = 1 - pred_z
@@ -30,6 +31,6 @@ def hinge(w,xTr,yTr,lambdaa):
     # gradient_over_samples = -np.multiply(yTr.T, xTr.T) # n x d
     # gradient_over_samples[np.argwhere(pred_z >= 1), :] = 0
     # gradient = np.sum(gradient_over_samples, axis=0) + (2 * lambdaa * w.T)
-    # gradient = (gradient.T / float(np.linalg.norm(gradient)))
+    #gradient = (gradient.T / float(np.linalg.norm(gradient)))
 
     return loss, gradient
