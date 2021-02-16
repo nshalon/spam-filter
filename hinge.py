45644a8a45387cde034bd34bc920824044d4a26b
Kenny Sherman <kennethsherm@gmail.com>
2021-02-15 19:31:08 -0600

    sherman hinge implementation

--- hinge.py
+++ hinge.py
@@ -16,14 +16,20 @@ def hinge(w,xTr,yTr,lambdaa):
 #
 # loss = the total loss obtained with w on xTr and yTr
 # gradient = the gradient at w
-    pred_z = np.multiply(yTr, (np.asmatrix(w.T) * xTr)) # 1 x n
-    emp_loss_nonzerod = 1 - pred_z
-    emp_loss = np.maximum(emp_loss_nonzerod, np.zeros(emp_loss_nonzerod.shape[1]))
-    loss = np.sum(emp_loss, axis=1) + lambdaa * (float(np.linalg.norm(w)) ** 2)
 
-    gradient_over_samples = -np.multiply(yTr.T, xTr.T) # n x d
-    gradient_over_samples[np.argwhere(pred_z >= 1), :] = 0
-    gradient = np.sum(gradient_over_samples, axis=0) + (2 * lambdaa * w.T)
+    pred_z = np.sum(xTr.T * w.T, axis=1) * yTr
+    gradient = lambdaa * np.sum((xTr * yTr) * (pred_z >= 1), axis=1) + (2 * w)
+
+    loss = lambdaa * np.sum(np.maximum(1 - pred_z, np.zeros(xTr.shape[1]))) + np.linalg.norm(w)
+
+    # pred_z = np.multiply(yTr, (np.asmatrix(w.T) * xTr)) # 1 x n
+    # emp_loss_nonzerod = 1 - pred_z
+    # emp_loss = np.maximum(emp_loss_nonzerod, np.zeros(emp_loss_nonzerod.shape[1]))
+    # loss = np.sum(emp_loss, axis=1) + lambdaa * (float(np.linalg.norm(w)) ** 2)
+    #
+    # gradient_over_samples = -np.multiply(yTr.T, xTr.T) # n x d
+    # gradient_over_samples[np.argwhere(pred_z >= 1), :] = 0
+    # gradient = np.sum(gradient_over_samples, axis=0) + (2 * lambdaa * w.T)
     # gradient = (gradient.T / float(np.linalg.norm(gradient)))
 
-    return loss, gradient.T
+    return loss, gradient
