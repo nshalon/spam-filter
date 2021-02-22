normalization fix

grdscent.py
@@ -19,12 +19,12 @@ def grdescent(func, w0, stepsize, maxiter, tolerance=1e-02):
         t += 1
         loss, gradient = func(w)
         if np.linalg.norm(gradient) <= np.linalg.norm(prev_gradient):
-            stepsize *= 1.01
+            stepsize *= 2.0
         else:
-            stepsize *= 0.5
-        if t % 1e2 == 0:
-            print("trial %s loss: %s" % (t, loss))
-            print('Gradient Norm: {}'.format(np.linalg.norm(gradient)))
+            stepsize *= 0.25
+        # if t % 1e2 == 0:
+            # print("trial %s loss: %s" % (t, loss))
+            # print('Gradient Norm: {}'.format(np.linalg.norm(gradient)))
         w = w - stepsize * gradient
         prev_gradient = gradient
 

hinge.py
@@ -18,12 +18,11 @@ def hinge(w,xTr,yTr,lambdaa):
 # gradient = the gradient at w
     pred_z = np.multiply(yTr, (np.asmatrix(w.T) * xTr)) # 1 x n
     emp_loss_nonzerod = 1 - pred_z
-    emp_loss = np.maximum(emp_loss_nonzerod, np.zeros(emp_loss_nonzerod.shape[1]))
-    loss = np.sum(emp_loss, axis=1) + lambdaa * (float(np.linalg.norm(w)) ** 2)
+    emp_loss = np.maximum(emp_loss_nonzerod, 0)
+    loss = np.sum(emp_loss, axis=1) + lambdaa * (np.linalg.norm(w) ** 2)
 
-    gradient_over_samples = -np.multiply(yTr.T, xTr.T) # n x d
-    gradient_over_samples[np.argwhere(pred_z >= 1), :] = 0
-    gradient = np.sum(gradient_over_samples, axis=0) + (2 * lambdaa * w.T)
-    # gradient = (gradient.T / float(np.linalg.norm(gradient)))
+    gradient_over_samples = np.multiply(-1*yTr.T, xTr.T) # n x d
+    gradient_over_samples[np.argwhere(pred_z.flatten() > 1), :] = 0
+    gradient = np.sum(gradient_over_samples, axis=0) + 2*(lambdaa * w.T)
 
     return loss, gradient.T

project1Main.py
@@ -13,11 +13,11 @@ Y = data['Y']
 # split the data:
 xTr, xTv, yTr, yTv = valsplit(X, Y)
 
-xTr_normed = xTr / xTr.max(axis=0)
-xTv_normed = xTv / xTv.max(axis=0)
+xTr_normed = xTr / xTr.sum(axis=0)
+xTv_normed = xTv / xTv.sum(axis=0)
 
 # train spam filter with settings and parameters in trainspamfilter.py
-w_trained = trainspamfilter(xTr_normed, yTr)
+w_trained = trainspamfilter(xTr_normed, yTr, 0)
 
 # evaluate spam filter on test set using default threshold
 spamfilter(xTv_normed, yTv, w_trained)